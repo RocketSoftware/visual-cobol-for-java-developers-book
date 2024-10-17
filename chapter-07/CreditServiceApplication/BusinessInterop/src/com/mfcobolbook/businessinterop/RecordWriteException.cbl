9ae1e6570c216602a8143b20b50d63a27698b23e
Sezgin Halibov <shalibov2@opentext.com>
2024-10-17 17:53:21 +0300

    Use Rocket Software branding

--- chapter-07/CreditServiceApplication/BusinessInterop/src/com/mfcobolbook/businessinterop/RecordWriteException.cbl
+++ chapter-07/CreditServiceApplication/BusinessInterop/src/com/mfcobolbook/businessinterop/RecordWriteException.cbl
@@ -1,21 +1,10 @@
-      *****************************************************************
-      *                                                               *
-      * Copyright 2020-2023 Open Text. All Rights Reserved.           *
-      * This software may be used, modified, and distributed          *
-      * (provided this notice is included without modification)       *
-      * solely for demonstration purposes with other                  *
-      * Micro Focus software, and is otherwise subject to the EULA at *
-      * https://www.microfocus.com/en-us/legal/software-licensing.    *
-      *                                                               *
-      * THIS SOFTWARE IS PROVIDED "AS IS" AND ALL IMPLIED           *
-      * WARRANTIES, INCLUDING THE IMPLIED WARRANTIES OF               *
-      * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE,         *
-      * SHALL NOT APPLY.                                              *
-      * TO THE EXTENT PERMITTED BY LAW, IN NO EVENT WILL              *
-      * MICRO FOCUS HAVE ANY LIABILITY WHATSOEVER IN CONNECTION       *
-      * WITH THIS SOFTWARE.                                           *
-      *                                                               *
-      *****************************************************************
+      ******************************************************************
+      *                                                                *
+      * Copyright 2020-2024 Rocket Software, Inc. or its affiliates.   *
+      * All Rights Reserved.                                           *
+      *                                                                *
+      ******************************************************************
+
       
        class-id com.mfcobolbook.businessinterop.RecordWriteException public
                 inherits type RuntimeException.               
